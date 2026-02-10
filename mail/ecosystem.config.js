021b6fa285836ad88d8859c7b85e86390a6de9d6
rohanre1813 <rohanshetty135781@gmail.com>
2026-02-10 14:55:07 +0530

    fixed jwt middleware typescript errors

---
+++ mail/ecosystem.config.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config(); // loads your .env
+
+export default {
+  apps: [
+    {
+      name: "mail-service",
+      script: "src/index.ts",
+      interpreter: "node",
+      node_args: "--loader ts-node/esm",
+      env: {
+        PORT: process.env.PORT || 5001,
+        RABBITMQ_HOST: process.env.RABBITMQ_HOST,
+        RABBITMQ_PORT: process.env.RABBITMQ_PORT,
+        RABBITMQ_USERNAME: process.env.RABBITMQ_USERNAME,
+        RABBITMQ_PASSWORD: process.env.RABBITMQ_PASSWORD,
+        EMAIL_USER: process.env.EMAIL_USER,
+        EMAIL_PASS: process.env.EMAIL_PASS,
+      },
+    },
+  ],
+};
