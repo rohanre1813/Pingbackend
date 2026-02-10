e7d3611ef783ec982831a8efcd68e5a737b3e013
rohanre1813 <rohanshetty135781@gmail.com>
2026-02-10 15:05:11 +0530

    fixed jwt middleware typescript errors

--- mail/ecosystem.config.cjs
+++ mail/ecosystem.config.cjs
@@ -1,21 +1,22 @@
-require('dotenv').config(); // load .env
-
 module.exports = {
   apps: [
     {
       name: "mail-service",
-      script: "src/index.ts",
-      interpreter: "node",
-      node_args: "--loader ts-node/esm",
+      script: "src/index.ts",           // TS entry point
+      interpreter: "node",              // run with Node
+      node_args: "--loader ts-node/esm", // ESM + TS support
+      cwd: "/home/ubuntu/Pingbackend/mail", // important: working directory
+      watch: false,
       env: {
-        PORT: process.env.PORT || 5001,
-        RABBITMQ_HOST: process.env.RABBITMQ_HOST,
-        RABBITMQ_PORT: process.env.RABBITMQ_PORT,
-        RABBITMQ_USERNAME: process.env.RABBITMQ_USERNAME,
-        RABBITMQ_PASSWORD: process.env.RABBITMQ_PASSWORD,
-        EMAIL_USER: process.env.EMAIL_USER,
-        EMAIL_PASS: process.env.EMAIL_PASS,
-      },
-    },
-  ],
+        NODE_ENV: "production",
+        PORT: "5001",
+        RABBITMQ_HOST: "13.60.51.231",
+        RABBITMQ_PORT: "5672",
+        RABBITMQ_USERNAME: "guest",
+        RABBITMQ_PASSWORD: "guest",
+        EMAIL_USER: "rohanshetty135781@gmail.com",
+        EMAIL_PASS: "jfbilnsezwhntyyb"
+      }
+    }
+  ]
 };
